bb7b560b2b207d06ce1081d9039f42a907531c92
MSDWY <964580790@qq.com>
2024-08-22 20:43:32 +0800

    Update cachematrix.R
    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -21,12 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
              getInverse = getInverse)
 }
 
+
 ## This function computes the inverse of the special "matrix" created by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         inv <- x$getInverse()
         if (!is.null(inv)) {
                 message("getting cached data")
@@ -36,4 +36,5 @@ cacheSolve <- function(x, ...) {
         inv <- solve(mat, ...)
         x$setInverse(inv)
         inv
+        ## Return a matrix that is the inverse of 'x'
 }
